16479352a76432d126e57c919c84279a7a9ea87a
arturios1916 <arturios1996@gmail.com>
2018-11-21 16:18:26 +0200

    fix bootstrap, add slick slider

---
+++ src/js/init.js
@@ -0,0 +1,11 @@
+//@prepros-append slick.min.js
+
+$(document).ready(function(){
+    $('.slider__wrapper').slick({
+     dots: true,
+     accessibility: false,
+     autoplay: false,
+     arrows: false,
+     draggable: false,
+    });
+  });
