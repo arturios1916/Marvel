db9d78f36acd4d52a4e16e85d7f12fc1bfcd7a07
arturios1916 <arturios1996@gmail.com>
2018-11-28 19:59:53 +0200

    footer section manipulation, finish map animation

--- src/js/init.js
+++ src/js/init.js
@@ -18,11 +18,13 @@ $('.map__captionwrap').click(function(){
   if (currentMap === 0) {
     $('.map').css('height','400px');
     $(this).css('height','50px');
+    $('.map__item').css('padding-top','50px');
     currentMap = 1;
   }
   else {
     $('.map').css('height','170px');
-    $(this).css('height','100%')
+    $(this).css('height','100%');
+    $('.map__item').css('padding-top','0px');
     currentMap = 0;
   }
 
