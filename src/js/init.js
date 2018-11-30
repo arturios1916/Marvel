cc103abb4d8e3db4b2bc6092a3ceb15abd218878
arturios1916 <arturios1996@gmail.com>
2018-11-30 21:56:25 +0200

    finish

--- src/js/init.js
+++ src/js/init.js
@@ -2,15 +2,35 @@
 
 $(document).ready(function(){
     $('.slider__wrapper').slick({
+     mobileFirst: true,
      dots: true,
      accessibility: false,
      autoplay: true,
      arrows: false,
      draggable: false,
-     touchMove: false,
+     touchMove: true,
      infinite: false,
      fade: true,
     });
+    $('.js-polo-carousel').slick({
+      arrows: true,
+      slidesToShow: 3,
+      responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 2,
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+            autoplay: true,
+          }
+        }
+      ]
+      })
   });
 
 var currentMap = 0;
@@ -22,10 +42,37 @@ $('.map__captionwrap').click(function(){
     currentMap = 1;
   }
   else {
-    $('.map').css('height','170px');
+    $('.map').css('height','');
     $(this).css('height','100%');
     $('.map__item').css('padding-top','0px');
     currentMap = 0;
   }
 
 })
+// scroll
+var section = document.getElementById('js-scrollItem');
+var coord = section.getBoundingClientRect().top;
+
+$('#js-scroll').click(function(){
+  $('html, body').animate({
+    scrollTop: section.getBoundingClientRect().top + "px",
+}, 700, 'swing');
+});
+
+//hamburger 
+var hamburger = document.querySelector(".hamburger");
+var nav = document.querySelector(".nav");
+
+hamburger.addEventListener("click", function() {
+    if (hamburger.classList.contains("is-active")) {
+      hamburger.classList.toggle("is-active");
+      nav.style='left:-100%;'
+      console.log(1);
+    }
+    else {
+      hamburger.classList.toggle("is-active");
+      nav.style='left:0%;  opacity: 1;'
+      console.log(2);
+    }
+
+});
